794f730d18fbf2d08c1393dba898fc4592c67420
RSV-exp <rapellysaivinil@gmail.com>
2026-06-30 15:33:20 +0530

    Adding the Candidate Retriver which used BucketSearch class to retrieve top 2000 most relevant candidates

---
+++ utils/CandidateRetriever.py
@@ -0,0 +1,34 @@
+from .build_metadata_filter import MetadataFilter
+from .BucketSearch import BucketSearch
+
+class CandidateRetriever:
+
+    def __init__(self):
+
+        self.metadata_filter = MetadataFilter()
+        self.bucket_search = BucketSearch()
+
+    def retrieve(self, response, collection_name):
+
+        candidate_pool = {}
+
+        filter_object = self.metadata_filter.filter_builder(
+            response.hard_rejects
+        )
+
+        buckets = [
+            ("bucket1", response.Bucket1),
+            ("bucket2", response.Bucket2)
+        ]
+
+        for bucket_name, statements in buckets:
+
+            candidate_pool = self.bucket_search.search_bucket(
+                bucket=statements,
+                metadata_filter=filter_object,
+                collection_name=collection_name,
+                bucket_name=bucket_name,
+                candidate_pool=candidate_pool
+            )
+
+        return candidate_pool
